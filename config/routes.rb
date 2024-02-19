53deb1d201bb08caf9cb232fda2cff7c6e558148
Simone D'Luna <simone.dluna@kellogg.northwestern.edu>
2024-02-19 02:36:30 +0000

    First bit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources "places"
 
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
