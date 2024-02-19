6df6a58f8a2aae06e2075f988054dd16a8db42df
Simone D'Luna <simone.dluna@kellogg.northwestern.edu>
2024-02-19 03:56:00 +0000

    Updates to create new html pages and functions

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,25 @@
 class PlacesController < ApplicationController
+  
+  # Page with list of places
   def index 
     @places = Place.all
+  end
+
+  # Form to create a new place
+  def new
+  end
+
+  #allow new data to be created
+  def create
+    @place=Place.new
+    @place["name"]=params["name"]
+    @place.save 
+    redirect_to "/places"
+  end
+
+  # Display one individual place
+  def show
+    @place = Place.find_by({"id" => params["id"]})
   end
 
 end
