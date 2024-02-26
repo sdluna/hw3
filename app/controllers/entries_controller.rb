cf434439879e8a13ddfa45690cbd954f4b0230ad
Simone D'Luna <simone.dluna@kellogg.northwestern.edu>
2024-02-26 00:15:39 +0000

    Fixed bug on date field and shifted landing page

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -17,7 +17,7 @@ class EntriesController < ApplicationController
     #data we enter
     @entry["title"]=params["title"]
     @entry["description"]=params["description"]
-    #@entry["date"]=params["date"]
+    @entry["posted_on"]=params["posted_on"]
     #tie to place
     @entry["place_id"]=params["place_id"]
     #save
